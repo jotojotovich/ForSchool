b1017826f2165df1c5f191caebb8db12da588239
veveel <laukart35@yandex.ru>
2014-10-04 14:35:23 +0400

    файлы проекта
    файлы проекта

---
+++ js/adjustment.js
@@ -0,0 +1,6 @@
+    /* document's properties*/
+    for (e in document)
+	document.write("document."+e+document[e]+"<br>");
+   /* END document's properties*/
+
+   //*****************************************//
---
+++ js/main.js
@@ -0,0 +1,44 @@
+function bgwhite()
+{
+	document.getElementsByTagName("body")[0].style.backgroundColor="#eee";
+	document.getElementsByTagName("body")[0].style.color="#333";
+	document.getElementById("art1").style.color="#333";
+}
+//********************************************************
+
+function bgblack()
+{
+	document.getElementsByTagName("body")[0].style.backgroundColor="#333";
+	document.getElementsByTagName("body")[0].style.color="#ccc";
+	document.getElementById("art1").style.color="#ccc";
+}
+//********************************************************
+
+function tab()
+{
+	document.write('<br><table><tr><td></td></tr></table>');
+}
+
+/***************************************************/
+var text = "Нажмите кнопку \"Заменить\" или \"Заменить все \" " ;
+
+/****************************************************/
+
+function ABC ()
+{
+var a = 123;
+return a;
+}
+
+/*******************************************************/
+
+/*************** OK *************************/
+
+function OpenWord()
+{
+                 
+ 				var mylink = document.getElementById("MyLink");
+                mylink.setAttribute("href", "../ReadinessPlan.xlsx");
+                mylink.click();
+
+}
