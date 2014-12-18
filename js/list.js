e9687ec0ee9e1098a6dd418d6e381a9e9aaf22b7
veveel <laukart35@yandex.ru>
2014-12-18 07:08:43 +0300

    added modal window by jQuery

--- js/list.js
+++ js/list.js
@@ -60,9 +60,12 @@
        	alert("The button sent: " + msg);
        	document.write(msg);
        }
-       /*
+       
+
+
+       
 Использование document.write в другом окне
-       */
+       
        var newWindow; //глобальная переменная
        function makeNewWindow()
        {
@@ -88,6 +91,31 @@
        confirm("Вы уверены?");
        prompt("Hello, do you know me?");       
        console.log(2*5);
+
+       /* *************** modal window ************************ */
+
+       $(document).ready(function() { // вся магия после загрузки страницы
+	$('a#go').click( function(event){ // ловим клик по ссылки с id="go"
+		event.preventDefault(); // выключаем стандартную роль элемента
+		$('#overlay').fadeIn(400, // сначала плавно показываем темную подложку
+		 	function(){ // после выполнения предыдущей анимации
+				$('#modal_form') 
+					.css('display', 'block') // убираем у модального окна display: none;
+					.animate({opacity: 1, top: '50%'}, 200); // плавно прибавляем прозрачность одновременно со съезжанием вниз
+		});
+	});
+	/* Закрытие модального окна, тут делаем то же самое но в обратном порядке */
+	$('#modal_close, #overlay').click(function(){ // ловим клик по крестику или подложке
+		$('#modal_form')
+			.animate({opacity: 0, top: '45%'}, 200,  // плавно меняем прозрачность на 0 и одновременно двигаем окно вверх
+				function(){ // после анимации
+					$(this).css('display', 'none'); // делаем ему display: none;
+					$('#overlay').fadeOut(400); // скрываем подложку
+				}
+			);
+	});
+});
+
 
 
 
